e8ba28bab71014897260caa4db5b569f46ce36ba
wideputin117 <takermanish7@gmail.com>
2023-12-27 00:29:07 +0530

    anagram

---
+++ anagram-function.js
@@ -0,0 +1,17 @@
+function anagram(val1, val2){
+    let newval = val1.split('');
+    let newval2 = val2.split('');
+    newval = newval.sort((a,b)=>a-b);
+    newval2 = newval2.sort((a,b)=>a-b);
+    if(newval.length != newval2.length){
+        return false;
+    }
+   for(let i = 0; i< newval.length; i++){
+        if(newval[i] != newval2[i]){
+            return false;
+        }
+        return true;
+   }
+}
+
+console.log(anagram("manish","mansh"))
