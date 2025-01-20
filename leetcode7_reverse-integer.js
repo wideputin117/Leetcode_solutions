6f0df358b4d6c6d3cfd84d5435f33237c59c24d8
wideputin117 <takermanish7@gmail.com>
2025-01-20 22:37:58 +0530

    leetcode 7 reverse integer

---
+++ leetcode7_reverse-integer.js
@@ -0,0 +1,23 @@
+/** questin link- https://leetcode.com/problems/reverse-integer/submissions/1514823196/ */
+
+
+var reverse = function(x) {
+ let min = Math.pow(-2,31)
+ console.log(min)
+ let max = Math.pow(2,31)-1
+ console.log(max)     
+let ans =0;
+ while(x !== 0){
+    let y = x % 10;
+    if(x <0 && y >0){
+        y = y-10;
+    };
+    if(ans < Math.floor(min/10+1) ||  ans > Math.floor(max/10)){
+        return 0;
+    };
+    ans = ans * 10 + y ;
+    x = (x -y)/10
+ }
+ return ans
+};
+console.log(reverse(-1463847412))
