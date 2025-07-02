47aee06f4264648fb1eb8597698a70ecbf7a30bd
wideputin117 <takermanish7@gmail.com>
2025-07-02 23:38:43 +0530

    longest consecutive sequence

--- arr_medium/longest_consecutive_secuence_in_near_o(n).js
+++ arr_medium/longest_consecutive_secuence_in_near_o(n).js
@@ -12,18 +12,23 @@ Output: 4
 Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4. */
 var longestConsecutive =(nums)=>{
     let ans =0;
-    let set = new Set(nums)
+    let set = new Set(nums.map(num => Math.abs(num)))
     let y
+    console.log(set)
     for(let i=0;i<nums.length-1;i++){
         if(!set.has(nums[i]-1)){
-        y = nums[i]+1
-       while(set.has(y)){
-        y+=1
+        let currentStreak =1
+        let currentNum =nums[i]
+       while(set.has(currentNum+1)){
+        currentNum++
+        currentStreak ++;
        }
-       ans = Math.max(ans,y-nums[i])
+       ans = Math.max(ans,currentStreak)
         }
     }
     return ans
 }
-console.log(longestConsecutive([0,3,7,2,5,8,4,6,0,1]))
+// console.log(longestConsecutive([0,3,7,2,5,8,4,6,0,1]))
+console.log(longestConsecutive([0,-1]))
 // console.log(data)
+ 
